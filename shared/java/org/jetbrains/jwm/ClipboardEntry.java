c18ca75e2c56afb0afa0d37f2170716e91c9ccc6
Egor Orachev <egororachyov5@gmail.com>
2021-08-31 15:03:18 +0300

    [Project] Add primary java api docs

--- shared/java/org/jetbrains/jwm/ClipboardEntry.java
+++ shared/java/org/jetbrains/jwm/ClipboardEntry.java
@@ -8,26 +8,58 @@ public class ClipboardEntry {
     @ApiStatus.Internal public final ClipboardFormat _format;
     @ApiStatus.Internal public final byte[] _data;
 
+    /**
+     * <p>Make new clipboard entry from format and raw byte data.</p>
+     *
+     * @param format        clipboard entry format
+     * @param data          raw byte data
+     * @return              clipboard entry
+     */
     @NotNull
     public static ClipboardEntry make(ClipboardFormat format, byte[] data) {
         return new ClipboardEntry(format, data);
     }
 
+    /**
+     * <p>Make new plain text entry from plain text.</p>
+     *
+     * @param text          raw string text
+     * @return              clipboard entry
+     */
     @NotNull @SneakyThrows
     public static ClipboardEntry makePlainText(String text) {
         return makeString(ClipboardFormat.TEXT, text);
     }
 
+    /**
+     * <p>Make new html entry from plain text.</p>
+     *
+     * @param text          raw string text
+     * @return              clipboard entry
+     */
     @NotNull @SneakyThrows
     public static ClipboardEntry makeHTML(String text) {
         return makeString(ClipboardFormat.HTML, text);
     }
 
+    /**
+     * <p>Make new rtf entry from plain text.</p>
+     *
+     * @param text          raw string text
+     * @return              clipboard entry
+     */
     @NotNull @SneakyThrows
     public static ClipboardEntry makeRTF(String text) {
         return makeString(ClipboardFormat.RTF, text);
     }
 
+    /**
+     * <p>Make new clipboard entry from format and plain text.</p>
+     *
+     * @param format        clipboard entry format
+     * @param text          raw string text
+     * @return              clipboard entry
+     */
     @NotNull @SneakyThrows
     public static ClipboardEntry makeString(ClipboardFormat format, String text) {
         if (Platform.CURRENT == Platform.X11) {
@@ -36,6 +68,12 @@ public class ClipboardEntry {
         return make(format, text.getBytes("UTF-16LE"));
     }
 
+    /**
+     * <p>Get clipboard entry content as string text.</p>
+     * <p>If data cannot be converted to the string, this function throws exception.</p>
+     *
+     * @return              clipboard data converted to the string
+     */
     @NotNull @SneakyThrows
     public String getString() {
         if (Platform.CURRENT == Platform.X11) {
