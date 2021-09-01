4e4f9223b09069045097bd1f19caf89351fb1132
ItoYo16u <ito.yo16uh90616@gmail.com>
2021-09-01 14:17:00 +0900

    indent

--- shared/cc/ThemeHelper.hh
+++ shared/cc/ThemeHelper.hh
@@ -1,11 +1,11 @@
 #pragma once
 
 namespace jwm {
-enum class Theme { Light, Dark };
+    enum class Theme { Light, Dark };
 
-class ThemeHelper {
-   public:
-    Theme getCurrentTheme();
-    Theme setTheme(Theme theme);
-};
+    class ThemeHelper {
+       public:
+           Theme getCurrentTheme();
+           Theme setTheme(Theme theme);
+    };
 }  // namespace jwm
--- windows/cc/ThemeHelperWin32.hh
+++ windows/cc/ThemeHelperWin32.hh
@@ -5,34 +5,34 @@
 #include "ThemeHelperWin32.hh"
 
 namespace jwm {
-typedef enum { WCA_USEDARKMODECOLORS = 26 } WINDOWCOMPOSITIONATTRIB;
+    typedef enum { WCA_USEDARKMODECOLORS = 26 } WINDOWCOMPOSITIONATTRIB;
 
-typedef struct {
-    WINDOWCOMPOSITIONATTRIB Attrib;
-    PVOID pvData;
-    SIZE_T cbData;
-} WINDOWCOMPOSITIONATTRIBDATA;
+    typedef struct {
+        WINDOWCOMPOSITIONATTRIB Attrib;
+        PVOID pvData;
+        SIZE_T cbData;
+    } WINDOWCOMPOSITIONATTRIBDATA;
 
-typedef NTSTATUS(WINAPI *RtlGetVersion_FUNC)(OSVERSIONINFOW *);
-typedef bool(WINAPI *ShouldAppsUseDarkMode)();
-typedef bool(WINAPI *SetWindowCompositionAttribute)(
-    HWND, WINDOWCOMPOSITIONATTRIBDATA *);
+    typedef NTSTATUS(WINAPI *RtlGetVersion_FUNC)(OSVERSIONINFOW *);
+    typedef bool(WINAPI *ShouldAppsUseDarkMode)();
+    typedef bool(WINAPI *SetWindowCompositionAttribute)(
+        HWND, WINDOWCOMPOSITIONATTRIBDATA *);
 
-class ThemeHelperWin32 final : public ThemeHelper {
-   public:
-    ThemeHelperWin32(HWND hWnd) { _hWnd = hWnd; };
-    ~ThemeHelperWin32() = default;
-    Theme getCurrentTheme();
-    Theme setTheme(Theme theme);
+    class ThemeHelperWin32 final : public ThemeHelper {
+       public:
+           ThemeHelperWin32(HWND hWnd) { _hWnd = hWnd; };
+           ~ThemeHelperWin32() = default;
+           Theme getCurrentTheme();
+           Theme setTheme(Theme theme);
 
-   private:
-    HWND _hWnd;
-    bool _checkOSVersion();
-    // check if OS is Windows 10 and build version >= 17763.
-    bool _isDarkModeSupported();
-    // If screen is high contrast mode, disable dark mode.
-    bool _isHicontrast();
-    // This function sets window's title bar light/dark.
-    bool _setThemeInternal(HWND hWnd, Theme theme, bool isDarkMode);
-};
+       private:
+           HWND _hWnd;
+           bool _checkOSVersion();
+           // check if OS is Windows 10 and build version >= 17763.
+           bool _isDarkModeSupported();
+           // If screen is high contrast mode, disable dark mode.
+           bool _isHicontrast();
+           // This function sets window's title bar light/dark.
+           bool _setThemeInternal(HWND hWnd, Theme theme, bool isDarkMode);
+    };
 }  // namespace jwm
