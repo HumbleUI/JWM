09c0cca513d34651fa9a38ab01d92863008e2529
mworzala <mattheworzala@gmail.com>
2022-01-11 01:14:57 -0500

    add _values to WindowMacTitlebarStyle

--- macos/java/WindowMacTitlebarStyle.java
+++ macos/java/WindowMacTitlebarStyle.java
@@ -3,5 +3,7 @@ package io.github.humbleui.jwm;
 public enum WindowMacTitlebarStyle {
     DEFAULT,
     UNIFIED_SMALL,
-    UNIFIED_LARGE
+    UNIFIED_LARGE;
+
+    @ApiStatus.Internal public static final WindowMacTitlebarStyle[] _values = values();
 }
