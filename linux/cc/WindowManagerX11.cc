5554c2c4f234b9fc81869b6ddfe6ea7cf946d742
navaratine <zbigniew.urban@livekid.com>
2022-09-17 11:56:32 +0200

    X11: Update mouse position in XI_Motion event (closes #241)

--- linux/cc/WindowManagerX11.cc
+++ linux/cc/WindowManagerX11.cc
@@ -416,6 +416,8 @@ void WindowManagerX11::_processXEvent(XEvent& ev) {
                             ::Window unused1;
                             int unused2;
                             XQueryPointer(display, myWindow->_x11Window, &unused1, &unused1, &unused2, &unused2, &unused2, &unused2, &mask);
+                            lastMousePosX = deviceEvent->event_x;
+                            lastMousePosY = deviceEvent->event_y;
                             jwm::JNILocal<jobject> eventMove(
                                 app.getJniEnv(),
                                 EventMouseMove::make(app.getJniEnv(),
