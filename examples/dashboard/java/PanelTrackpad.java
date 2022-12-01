c037d5fad82eb981d82dfe25220a1fabc87064f1
shaun <shaunewilliams@gmail.com>
2022-12-01 12:42:11 -0600

    track touches and devices in trackpad panel

--- examples/dashboard/java/PanelTrackpad.java
+++ examples/dashboard/java/PanelTrackpad.java
@@ -9,12 +9,27 @@ import io.github.humbleui.skija.*;
 import io.github.humbleui.types.*;
 
 public class PanelTrackpad extends Panel {
+    public Map<Integer, Point> touches = Collections.synchronizedMap(new HashMap<Integer, Point>()); // coords
+    public Map<Integer, Point> devices = Collections.synchronizedMap(new TreeMap<Integer, Point>()); // sizes
+    public Map<Integer, Integer> touchDevices = Collections.synchronizedMap(new HashMap<Integer, Integer>()); // touch -> device
+
     public PanelTrackpad(Window window) {
         super(window);
     }
 
     @Override
     public void accept(Event e) {
+        if (e instanceof EventTrackpadTouchStart ee) {
+            touchDevices.put(ee.getDeviceId(), ee.getId());
+            devices.put(ee.getDeviceId(), new Point(ee.getDeviceWidth(), ee.getDeviceHeight()));
+            touches.put(ee.getId(), new Point(ee.getFracX(), ee.getFracY()));
+        } else if (e instanceof EventTrackpadTouchMove ee) {
+            touches.put(ee.getId(), new Point(ee.getFracX(), ee.getFracY()));
+        } else if (e instanceof EventTrackpadTouchCancel ee) {
+            touches.remove(ee.getId());
+        } else if (e instanceof EventTrackpadTouchEnd ee) {
+            touches.remove(ee.getId());
+        }
     }
 
     @Override
