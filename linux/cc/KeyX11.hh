81e3666d697534a2a2489561ecf88e9bb926ecb6
dzaima <dzaimagit@gmail.com>
2023-12-04 01:01:20 +0200

    X11: add KeyLocation, additional keys

--- linux/cc/KeyX11.hh
+++ linux/cc/KeyX11.hh
@@ -2,10 +2,11 @@
 
 #include <cstdint>
 #include "Key.hh"
+#include "KeyLocation.hh"
 
 namespace jwm {
     namespace KeyX11 {
-        jwm::Key fromNative(uint32_t v);
+        jwm::Key fromNative(uint32_t v, jwm::KeyLocation& location, int& modifiers);
         bool getKeyState(jwm::Key key);
         void setKeyState(jwm::Key key, bool isDown);
         int getModifiers();
