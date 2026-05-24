daf78616773955eed5732264de09968f8a1de941
Tue Ton <49886739+chirontt@users.noreply.github.com>
2026-05-24 18:57:19 -0400

    add loading of jwm_arm64.dll for Windows Arm64

--- shared/java/impl/Library.java
+++ shared/java/impl/Library.java
@@ -41,7 +41,8 @@ public class Library {
             File library = _extract("/", file, tempDir);
             System.load(library.getAbsolutePath());
         } else if (Platform.CURRENT == Platform.WINDOWS) {
-            File library = _extract("/", "jwm_x64.dll", tempDir);
+            String file = "aarch64".equals(System.getProperty("os.arch")) ? "jwm_arm64.dll" : "jwm_x64.dll";
+            File library = _extract("/", file, tempDir);
             System.load(library.getAbsolutePath());
         } else if (Platform.CURRENT == Platform.X11) {
             File library = _extract("/", "libjwm_x64.so", tempDir);
